202f92e7cd906153ec39c45d85846673b86a4832
Vladislav Naumov <Naum6666@yandex.ru>
2025-01-21 18:16:27 +0300

    Исправления

--- Assets/Script/Controllers/Inventory/DragAndDropItem.cs
+++ Assets/Script/Controllers/Inventory/DragAndDropItem.cs
@@ -1,8 +1,4 @@
 using Assets.Script;
-using Assets.Script.GUI;
-using Assets.Script.Items;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -11,15 +7,6 @@ public class DragAndDropItem : MonoBehaviour, IPointerDownHandler, IDragHandler,
 	private Vector2 currentPosition;
 	public Inventory inventory;
 	public CollectableItem collectable;
-	private Vector3 MouseWorldPosition
-	{
-		get
-		{
-			var mouseScreenPos = Input.mousePosition;
-			mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
-			return Camera.main.WorldToScreenPoint(mouseScreenPos);
-		}
-	}
 
 	private void Awake()
 	{
@@ -33,30 +20,9 @@ public class DragAndDropItem : MonoBehaviour, IPointerDownHandler, IDragHandler,
 	public void OnEndDrag(PointerEventData eventData)
 	{
 		transform.localPosition = currentPosition;
-
 	}
 	public void OnPointerDown(PointerEventData eventData)
 	{
 		currentPosition = new Vector2(transform.localPosition.x, transform.localPosition.y);
-	}
-	public void OnMouseUp()
-	{
-		Collider2D collider = GetComponent<Collider2D>();
-
-		collider.enabled = false;
-		var rayOrigin = Camera.main.transform.position;
-		var rayDirection = MouseWorldPosition - rayOrigin;
-		RaycastHit2D hit;
-
-		if (hit = Physics2D.Raycast(rayOrigin, rayDirection))
-		{
-			if (hit.transform.CompareTag("SlotTrash"))
-			{
-				var item = collectable.item;
-				inventory.RemoveItem(item);
-			}
-		}
-
-		collider.enabled = false;
 	}
 }
---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Script.Items;
+
+namespace Assets.Script
+{
+	public class Inventory : MonoBehaviour
+	{
+		public GameObject canvas; // Панель инвентаря
+		public GameObject inventoryPanel; // Панель слотов инвентаря
+		public GameObject equipmentPanel; // Панель слотов снаряжения
+		public Dictionary<EquipmentSlot, CollectableItem> equipment = new();
+
+		private readonly List<Item> items = new(); // Список предметов в инвентаре
+		public int slotCount => 2;
+
+		void Start()
+		{
+			canvas.SetActive(false); // Скрыть инвентарь при старте
+		}
+
+		public void OnClick()
+		{
+			if(!canvas.activeSelf)
+				UpdateInventoryUI();
+			canvas.SetActive(!canvas.activeSelf);
+		}
+
+		// Добавление предмета в инвентарь
+		public bool AddItem(CollectableItem collectable)
+		{
+			if(items.Count < slotCount)
+			{
+				items.Add(collectable.item);
+				UpdateInventoryUI();
+				return true;
+			}
+			return false;
+		}
+		public void RemoveItem(CollectableItem collectable)
+		{
+			items.Remove(collectable.item);
+			UpdateInventoryUI();	
+		}
+		public void SetEquipment(CollectableItem collectable)
+		{
+			EquipmentSlot slot = collectable.Slot;
+			equipment[slot] = collectable;
+		}
+		public void RemoveEquipment(CollectableItem collectable)
+		{
+			EquipmentSlot slot = collectable.Slot;
+			equipment[slot] = null;
+		}
+		// Обновление UI инвентаря
+		void UpdateInventoryUI()
+		{
+			foreach (Transform slot in inventoryPanel.transform)
+			{
+				slot.GetChild(0).GetComponent<Image>().sprite = null;
+				slot.GetChild(0).GetComponent<CollectableItem>().SetItem(Item.Empty);
+			}
+
+			int i = 0;
+			foreach (Item item in items)
+			{
+				Transform slot = inventoryPanel.transform.GetChild(i++);
+				slot.GetChild(0).GetComponent<Image>().sprite = item.icon;
+				slot.GetChild(0).GetComponent<CollectableItem>().SetItem(item);
+			}
+		}
+	}
+}
